f9c303bfbfeff2e65b816f64eca87e7e68595417
sbhusal123 <suryabhusal11@gmail.com>
2017-10-28 15:28:28 +0545

    ScrollPane added for overflow

--- src/javasocket/Controller.java
+++ src/javasocket/Controller.java
@@ -1,18 +1,23 @@
 package javasocket;
 
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.TextAlignment;
 
 public class Controller {
 
     @FXML
-    VBox converstation;
+    ScrollPane messages;
 
+    VBox messages_vbox = new VBox();
+    
     @FXML
     TextField message;
 
@@ -25,14 +30,23 @@ public class Controller {
 
         set_text.setStyle("-fx-padding:10;-fx-margin:10;"
                 + "-fx-background-color:teal;"
-                + "    -fx-background-insets: 5;"
+                + "-fx-background-insets: 5;"
                 + "-fx-font-size:15;"
                 + "-fx-background-radius: 3;");
         set_text.setPrefSize(Double.MAX_VALUE, Double.compare(0, 500));
                set_text.setWrapText(true);
         set_text.setTextAlignment(TextAlignment.JUSTIFY);
-        converstation.getChildren().add(set_text);
-
+        
+        
+        //vbox
+        messages_vbox.getChildren().add(set_text);
+        messages_vbox.setStyle("-fx-background-color:teal");
+        
+        messages.setVbarPolicy(ScrollBarPolicy.ALWAYS);
+        messages.setHbarPolicy(ScrollBarPolicy.NEVER);
+        
+        //scrollpane
+        messages.setContent(messages_vbox);
         message.setText("");
 
     }
